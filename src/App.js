556ee261abed5c827d8642a316265c5c87720d6b
coolistkovbel <coolistkovbel@gmail.com>
2022-07-03 02:48:29 -0400

    basic react setup ready for metamask connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ether } from "ethers";
 
 function App() {
+  const handleClick = () => {
+    alert("connect metamask");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PdeX</h1>
       </header>
+      <section className="hero">
+        <p>A place to go solo or group up</p>
+        <button onClick={handleClick}>Connect Now</button>
+      </section>
+      <footer>
+        <p>
+          Made with thought{" "}
+          <a href="#" target="_blank">
+            @ckovbel
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
